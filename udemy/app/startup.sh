732c6996f4f063233dbedde0101e53d70e828638
João Pedro <jpedrones@hotmail.com>
2024-11-23 13:00:08 -0300

    [app]: fixed database connection bug Fixed a bug where the app started before the database.

---
+++ udemy/app/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "[======== PINGING DATABASE CONTAINER ========]"
+
+start_time=$(date +%s)
+end_time=$((start_time + 30))
+
+while [ $(date +%s) -lt $end_time ]; do
+
+	nc -zv spring_db 3306
+	if [ $? -eq 0 ]; then
+		break
+	fi
+	sleep 1
+done
+
+if [ $(date +%s) -ge $end_time ]; then 
+	echo "[======== DATABASE IS NOT RESPONDING ========]"
+else
+	java -jar /spring-boot-study.jar
+fi
